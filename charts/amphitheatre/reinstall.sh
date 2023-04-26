289509c81b37df4639c1e907036b1d11fcf35edb
Eguo Wang <wangeguo@gmail.com>
2023-04-26 15:10:32 +0800

    Update NodePort shell and reinstall script

--- charts/amphitheatre/reinstall.sh
+++ charts/amphitheatre/reinstall.sh
@@ -10,12 +10,10 @@ helm dependency build
 echo "2/4 Uninstall and delete Helm release if it already exists"
 helm uninstall amp --namespace amp-system
 kubectl delete ns amp-system
+sleep 30
 
 echo "3/4 Install new release"
-helm install amp . --create-namespace --namespace amp-system &&
-
-echo "4/4 Print post-installation notes, and perform port forwarding"
-export POD_NAME=$(kubectl get pods --namespace amp-system -l "app.kubernetes.io/name=amp-apiserver,app.kubernetes.io/instance=amp" -o jsonpath="{.items[0].metadata.name}") &&
-export CONTAINER_PORT=$(kubectl get pod --namespace amp-system $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}") &&
-echo "Visit http://127.0.0.1:8170 to use your application" &&
-kubectl --namespace amp-system port-forward $POD_NAME 8170:$CONTAINER_PORT
+helm install amp . --create-namespace --namespace amp-system \
+    --set service.type=NodePort \
+    --set apiserver.debug=true \
+    --set controllers.debug=true
